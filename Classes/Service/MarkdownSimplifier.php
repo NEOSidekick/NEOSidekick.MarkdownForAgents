d8dedf859b57de2390e21e90ebe8aabae408a990
Felix Gradinaru <felix@gradinaru.me>
2026-06-09 16:05:07 +0200

    BUGFIX: Separate atx headings fused to preceding inline text
    league/html-to-markdown emits no leading newline before an atx heading, so a heading following inline content without a block boundary (a label before an <h1>, bold or an image before a heading) lands mid-line ("text# Heading") and no longer renders as a heading. Insert a paragraph break before a mid-line heading marker. The ([\w]{2}|[^\w\s]) guard avoids splitting inline tokens such as "C#"/"F#" that are a single letter followed by the marker.

--- Classes/Service/MarkdownSimplifier.php
+++ Classes/Service/MarkdownSimplifier.php
@@ -11,6 +11,7 @@ final class MarkdownSimplifier
         $markdown = html_entity_decode($markdown, ENT_QUOTES | ENT_HTML5, 'UTF-8');
         $markdown = str_replace("\xc2\xa0", ' ', $markdown);
         $markdown = preg_replace('/[ \t]+\n/', "\n", $markdown) ?? $markdown;
+        $markdown = preg_replace('/([\w]{2}|[^\w\s])(#{1,6} )/', "$1\n\n$2", $markdown) ?? $markdown;
         $markdown = preg_replace('/\n{3,}/', "\n\n", $markdown) ?? $markdown;
         $markdown = preg_replace('/^[-\>][ \t]*\n/m', '', $markdown) ?? $markdown;
 
